23e06e99abdefd0b8b0aea3d975eb2e29636120b
ankitpreet <98426860+ankitpreet@users.noreply.github.com>
2022-04-12 06:24:40 -0400

    lab5

---
+++ powershell/Lab5.ps1
@@ -0,0 +1,18 @@
+﻿param ( [switch]$System, [switch]$Disks, [switch]$Network)
+
+Import-Module ankitpreetsharma
+
+if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
+	write-Host "Include CMD Args. Otherwise this script created to display/run all possible functions."
+	write-Host "______________________________________________________________________________________"
+	write-Host ""
+  ankitpreetsharma-System;ankitpreetsharma-Disks;ankitpreetsharma-Network
+  write-Host ""
+} else {
+  if ($System) { ankitpreetsharma-System;
+  }
+  if ($Disks) { ankitpreetsharma-Disks;
+  }
+  if ($Network) { ankitpreetsharma-Network;
+  }
+}
